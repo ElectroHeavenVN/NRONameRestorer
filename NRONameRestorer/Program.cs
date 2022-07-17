055372899128f9a7d6064ebb1ed2dedaa6e9598e
ElectroHeavenVN <dinhcongthanhhs@gmail.com>
2022-07-17 12:55:42 +0700

    Add project files.

---
+++ NRONameRestorer/Program.cs
@@ -0,0 +1,184 @@
+﻿using dnlib.DotNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace NRONameRestorer
+{
+    internal class Program
+    {
+        [DllImport("msvcrt.dll")]
+        static extern bool system(string cmd);
+
+        static void pause() => system("pause");
+
+        static ModuleDef originalModule, obfuscatedModule;
+
+        static uint skippedTypesCount;
+
+        static IList<TypeDef> unRenamedTypes = new List<TypeDef>();
+
+        static IList<TypeDef> unRenamedInterfaces = new List<TypeDef>();
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string originalModulePath;
+            string obfuscatedModulePath;
+            if (args.Length > 2)
+            {
+                Console.WriteLine("Nhiều hơn 2 tệp trong đối số dòng lệnh!");
+                pause();
+                return;
+            }
+            if (args.Length == 0)
+            {
+                Console.Write("Đường dẫn tệp bị mã hóa: ");
+                obfuscatedModulePath = Console.ReadLine().Replace("\"", "");
+            }
+            else obfuscatedModulePath = args[0];
+            if (args.Length <= 1)
+            {
+                Console.Write("Đường dẫn tệp gốc: ");
+                originalModulePath = Console.ReadLine().Replace("\"", "");
+            }
+            else originalModulePath = args[1];
+            obfuscatedModule = AssemblyDef.Load(obfuscatedModulePath).ManifestModule;
+            originalModule = AssemblyDef.Load(originalModulePath).ManifestModule;
+            RestoreName(obfuscatedModule.Types);
+            RestoreImplementedMethodsName();
+            RestoreUnrenamedInterfaceName();
+            string outputfile = Path.GetFileNameWithoutExtension(obfuscatedModulePath) + "-NameRestored" + Path.GetExtension(obfuscatedModulePath);
+            obfuscatedModule.Write(outputfile);
+            Console.WriteLine("Tệp đã phục hồi tên được lưu tại: " + outputfile);
+            pause();
+        }
+
+        private static void RestoreUnrenamedInterfaceName()
+        {
+            foreach (TypeDef type in unRenamedInterfaces)
+            {
+                TypeDef typeDef = originalModule.Find(type.FullName, false);
+                if (typeDef == null)
+                {
+                    Console.WriteLine($"Interface không được phục hồi tên: {type.FullName} [0x{type.MDToken}]");
+                    continue;
+                }
+                for (int i = 0; i < type.Methods.Count; i++)
+                {
+                    type.Methods[i].Name = typeDef.Methods[i].Name;
+                    for (int j = 0; j < type.Methods[i].Parameters.Count; j++)
+                    {
+                        type.Methods[i].Parameters[j].Name = typeDef.Methods[i].Parameters[j].Name;
+                    }
+                }
+            }
+        }
+
+        private static void RestoreImplementedMethodsName()
+        {
+            foreach (TypeDef type in unRenamedTypes)
+            {
+                if (type.HasInterfaces)
+                {
+                    foreach (InterfaceImpl interfaceImpl in type.Interfaces)
+                    {
+                        TypeDef typeDef = interfaceImpl.Interface.ResolveTypeDef();
+                        if (typeDef == null) continue;
+                        TypeDef typeDef1 = originalModule.Find(typeDef.FullName, false);
+                        if (typeDef1 == null) continue;
+                        for (int i = 0; i < typeDef1.Methods.Count; i++)
+                        {
+                            MethodDef methodDef = type.FindMethod(typeDef.Methods[i].Name, typeDef.Methods[i].MethodSig);
+                            methodDef.Name = typeDef1.Methods[i].Name;
+                            for (int j = 0; j < typeDef1.Methods[i].Parameters.Count; j++)
+                            {
+                                methodDef.Parameters[j].Name = typeDef1.Methods[i].Parameters[j].Name;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void RestoreName(IList<TypeDef> types)
+        {
+            foreach (TypeDef typeObf in types)
+            {
+                uint mdToken = typeObf.MDToken.ToUInt32();
+                bool isCompilerGen = false;
+                bool isRenamed = false;
+                bool isDontAdd = false;
+                if (typeObf.HasCustomAttributes)
+                {
+                    foreach (CustomAttribute customAttribute in typeObf.CustomAttributes)
+                    {
+                        if (customAttribute.AttributeType.FullName == "System.Runtime.CompilerServices.CompilerGeneratedAttribute")
+                        {
+                            isCompilerGen = true;
+                            skippedTypesCount++;
+                            break;
+                        }                       
+                    }
+                }
+                if (originalModule.Types.TypeExits(mdToken - skippedTypesCount, out TypeDef typeOri) && typeObf.isSame(typeOri) && !isCompilerGen)
+                {
+                    if (typeObf.Name != typeOri.Name) typeObf.Name = typeOri.Name;
+                    typeObf.Namespace = typeOri.Namespace;
+                    if (!typeObf.IsInterface && !typeOri.IsInterface) isRenamed = true;
+                    else isDontAdd = true;
+                }
+                if (isRenamed)
+                {
+                    int c = 0;
+                    int methodsCount = typeObf.Methods.Count < typeOri.Methods.Count ? typeObf.Methods.Count : typeOri.Methods.Count;
+                    for (; c < methodsCount; c++)
+                    {
+                        if (typeObf.Methods[c].HasOverrides) foreach (MethodOverride method in typeObf.Methods[c].Overrides) method.MethodBody.Name = typeOri.Methods[c].Name;
+                        typeObf.Methods[c].Name = typeOri.Methods[c].Name;
+                        for (int num2 = 0; num2 < typeObf.Methods[c].Parameters.Count; num2++)
+                        {
+                            typeObf.Methods[c].Parameters[num2].Name = typeOri.Methods[c].Parameters[num2].Name;
+                        }
+                    }
+                    if (c < typeObf.Methods.Count) for (; c < typeObf.Methods.Count; c++)
+                        {
+                            Console.WriteLine($"Hàm không được phục hồi tên: {typeObf.Methods[c].FullName} [0x{typeObf.Methods[c].MDToken}]");
+                        }
+                    int count2 = typeOri.Fields.Count < typeObf.Fields.Count ? typeOri.Fields.Count : typeObf.Fields.Count;
+                    int l;
+                    for (l = 0; l < count2; l++)
+                    {
+                        typeObf.Fields[l].Name = typeOri.Fields[l].Name;
+                    }
+                    if (l < typeObf.Fields.Count) for (; l < typeObf.Fields.Count; l++) Console.WriteLine($"Biến không được phục hồi tên: {typeObf.Fields[l].FullName} [0x{typeObf.Fields[l].MDToken}]");
+                }
+                else if (!isDontAdd)
+                {
+                    string str = string.Empty;
+                    if (typeObf.IsDelegate) str = "Delegate";
+                    else if (typeObf.IsEnum) str = "Enum";
+                    else if (typeObf.IsValueType) str = "Struct";
+                    else if (typeObf.IsInterface) str = "Interface";
+                    else if (typeObf.IsClass) str = "Class";
+                    Console.WriteLine($"{str} không được phục hồi tên: {typeObf.FullName} [0x{typeObf.MDToken}]");
+                    unRenamedTypes.Add(typeObf);
+                }
+                if (isDontAdd) unRenamedInterfaces.Add(typeObf);
+                if (typeObf.HasNestedTypes)
+                {
+                    if (isCompilerGen)
+                    {
+                        skippedTypesCount += (uint)typeObf.NestedTypes.Count;
+                    }
+                    else
+                    {
+                        RestoreName(typeObf.NestedTypes);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NRONameRestorer/Utils.cs
@@ -0,0 +1,134 @@
+﻿using dnlib.DotNet;
+using System.Collections.Generic;
+
+namespace NRONameRestorer
+{
+    public static class Utils
+    {
+        public static bool TypeExits(this IList<TypeDef> types, uint mdToken, out TypeDef typeDef)
+        {
+            typeDef = null;
+            foreach (TypeDef type in types)
+            {
+                TypeDef typeDef1 = type;
+                if (typeDef1.HasNestedTypes) TypeExits(type.NestedTypes, mdToken, out typeDef1);
+                if (typeDef1 == null) typeDef1 = type;
+                if (typeDef1.MDToken.ToUInt32() == mdToken)
+                {
+                    typeDef = typeDef1;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static TypeSig GetRootTypeSig(this TypeSig typeSig)
+        {
+            if (typeSig.Next == null) return typeSig;
+            else return GetRootTypeSig(typeSig.Next);
+        }
+
+        public static bool isSame(this TypeDef type, TypeDef typeDef)
+        {
+            return
+                type.HasClassLayout == typeDef.HasClassLayout &&
+                type.HasEvents == typeDef.HasEvents &&
+                type.HasFields == typeDef.HasFields &&
+                type.HasGenericParameters == typeDef.HasGenericParameters &&
+                type.HasInterfaces == typeDef.HasInterfaces &&
+                type.HasMethods == typeDef.HasMethods &&
+                //type.HasNestedTypes == typeDef.HasNestedTypes &&
+                type.HasProperties == typeDef.HasProperties &&
+                type.IsAbstract == typeDef.IsAbstract &&
+                type.IsAnsiClass == typeDef.IsAnsiClass &&
+                type.IsAutoClass == typeDef.IsAutoClass &&
+                type.IsAutoLayout == typeDef.IsAutoLayout &&
+                //type.IsBeforeFieldInit == typeDef.IsBeforeFieldInit &&
+                type.IsClass == typeDef.IsClass &&
+                type.IsCustomFormatClass == typeDef.IsCustomFormatClass &&
+                type.IsDelegate == typeDef.IsDelegate &&
+                type.IsEnum == typeDef.IsEnum &&
+                type.IsEquivalent == typeDef.IsEquivalent &&
+                type.IsExplicitLayout == typeDef.IsExplicitLayout &&
+                type.IsForwarder == typeDef.IsForwarder &&
+                type.IsGlobalModuleType == typeDef.IsGlobalModuleType &&
+                type.IsImport == typeDef.IsImport &&
+                type.IsInterface == typeDef.IsInterface &&
+                type.IsNested == typeDef.IsNested &&
+                type.IsNestedAssembly == typeDef.IsNestedAssembly &&
+                type.IsNestedFamily == typeDef.IsNestedFamily &&
+                type.IsNestedFamilyAndAssembly == typeDef.IsNestedFamilyAndAssembly &&
+                type.IsNestedFamilyOrAssembly == typeDef.IsNestedFamilyOrAssembly &&
+                type.IsNestedPrivate == typeDef.IsNestedPrivate &&
+                type.IsNestedPublic == typeDef.IsNestedPublic &&
+                type.IsNotPublic == typeDef.IsNotPublic &&
+                type.IsPrimitive == typeDef.IsPrimitive &&
+                type.IsPublic == typeDef.IsPublic &&
+                type.IsRuntimeSpecialName == typeDef.IsRuntimeSpecialName &&
+                type.IsSealed == typeDef.IsSealed &&
+                type.IsSequentialLayout == typeDef.IsSequentialLayout &&
+                type.IsSerializable == typeDef.IsSerializable &&
+                type.IsSpecialName == typeDef.IsSpecialName &&
+                type.IsUnicodeClass == typeDef.IsUnicodeClass &&
+                type.IsValueType == typeDef.IsValueType &&
+                type.IsWindowsRuntime == typeDef.IsWindowsRuntime;
+        }
+
+        public static bool isSame(this MethodDef method, MethodDef methodDef)
+        {
+            return
+                method.ExplicitThis == methodDef.ExplicitThis &&
+                method.HasBody == methodDef.HasBody &&
+                method.HasOverrides == methodDef.HasOverrides &&
+                method.HasParamDefs == methodDef.HasParamDefs &&
+                method.HasParams() == methodDef.HasParams() &&
+                method.HasReturnType == methodDef.HasReturnType &&
+                method.HasThis == methodDef.HasThis &&
+                method.IsAbstract == methodDef.IsAbstract &&
+                method.IsAddOn == methodDef.IsAddOn &&
+                method.IsAggressiveInlining == methodDef.IsAggressiveInlining &&
+                method.IsAggressiveOptimization == methodDef.IsAggressiveOptimization &&
+                method.IsAssembly == methodDef.IsAssembly &&
+                method.IsCheckAccessOnOverride == methodDef.IsCheckAccessOnOverride &&
+                method.IsCompilerControlled == methodDef.IsCompilerControlled &&
+                method.IsConstructor == methodDef.IsConstructor &&
+                method.IsFamily == methodDef.IsFamily &&
+                method.IsFamilyAndAssembly == methodDef.IsFamilyAndAssembly &&
+                method.IsFamilyOrAssembly == methodDef.IsFamilyOrAssembly &&
+                method.IsFinal == methodDef.IsFinal &&
+                method.IsFire == methodDef.IsFire &&
+                method.IsForwardRef == methodDef.IsForwardRef &&
+                method.IsGetter == methodDef.IsGetter &&
+                method.IsHideBySig == methodDef.IsHideBySig &&
+                method.IsIL == methodDef.IsIL &&
+                method.IsInstanceConstructor == methodDef.IsConstructor &&
+                method.IsInternalCall == methodDef.IsInternalCall &&
+                method.IsManaged == methodDef.IsManaged &&
+                method.IsNative == methodDef.IsNative &&
+                method.IsNewSlot == methodDef.IsNewSlot &&
+                method.IsNoInlining == methodDef.IsNoInlining &&
+                method.IsNoOptimization == methodDef.IsNoOptimization &&
+                method.IsOPTIL == methodDef.IsOPTIL &&
+                method.IsOther == methodDef.IsOther &&
+                method.IsPinvokeImpl == methodDef.IsPinvokeImpl &&
+                method.IsPreserveSig == methodDef.IsPreserveSig &&
+                method.IsPrivate == methodDef.IsPrivate &&
+                method.IsPrivateScope == methodDef.IsPrivateScope &&
+                method.IsPublic == methodDef.IsPublic &&
+                method.IsRemoveOn == methodDef.IsRemoveOn &&
+                method.IsRequireSecObject == methodDef.IsRequireSecObject &&
+                method.IsReuseSlot == methodDef.IsReuseSlot &&
+                method.IsRuntime == methodDef.IsRuntime &&
+                method.IsRuntimeSpecialName == methodDef.IsRuntimeSpecialName &&
+                method.IsSetter == methodDef.IsSetter &&
+                method.IsSpecialName == methodDef.IsSpecialName &&
+                method.IsStatic == methodDef.IsStatic &&
+                method.IsStaticConstructor == methodDef.IsStaticConstructor &&
+                method.IsSynchronized == methodDef.IsSynchronized &&
+                method.IsUnmanaged == methodDef.IsUnmanaged &&
+                method.IsUnmanagedExport == methodDef.IsUnmanagedExport &&
+                method.IsVirtual == methodDef.IsVirtual &&
+                method.Parameters.Count == methodDef.Parameters.Count;
+        }
+    }
+}
